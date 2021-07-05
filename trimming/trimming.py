fb3a949d18f369d7d0da0d89e9891ce74a5dc400
conf8o <41929738+conf8o@users.noreply.github.com>
2021-07-05 13:42:25 +0900

    docstring of trimming

--- trimming/trimming.py
+++ trimming/trimming.py
@@ -1,11 +1,20 @@
 from PIL import Image
 
 def resize_shorter(size: tuple, length: int) -> tuple:
+    """
+    サイズのタプル(height, width) の短いほうについて、縦横比を保ちながらリサイズする。
+    例: resize_shorter((42, 36), 6) なら 短いほうは 36 なので、そっちを 6 にし、縦横比を合わせる。-> (7, 6)
+    """
+
     long, short = size if size[0] > size[1] else reversed(size)
     after = (int(length * (long / short)), length)
-    return after if size[0] > size[1] else reversed(size)
+    return after if size[0] > size[1] else reversed(after)
 
 def crop_center(size: tuple, crop_size: tuple) -> tuple:
+    """
+    中心をcrop_sizeで切り抜く座標情報(左, 上, 右, 下)を取得する。
+    """
+    
     (left, right), (upper, lower) = (((a - b) // 2, (a + b) // 2) for a, b in zip(size, crop_size))
     return (left, upper, right, lower)
 
